9923ca64cc6496be70077549b7b2c77a7c8b563e
Michael Wood <mlwood952@gmail.com>
2020-10-12 11:32:47 -0400

    Add project files.

---
+++ DrinkApp/Drink.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DrinkApp
+{
+    class Drink
+    {
+        //private instance variable
+        private string mornDrink;
+
+        //setter
+        public void SetDrink (string drinkName) 
+        {
+            mornDrink = drinkName;
+        }
+
+        //getter
+        public string GetDrink()
+        {
+            return mornDrink;
+        }
+
+
+    }
+
+}
---
+++ DrinkApp/DrinkTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace DrinkApp
+{
+    class DrinkTest
+    {
+        static void Main(string[] args)
+        {
+            //create a new object
+            Drink mondayDrink = new Drink(); 
+
+            //initial value
+            Console.WriteLine($"Monday's favorite drink is: {mondayDrink.GetDrink()}"); 
+
+            // prompt
+            Console.Write("What is your favorite morning drink today:           "); 
+
+            //store drink name
+            String favDrink = Console.ReadLine();
+
+            // put drink name in object
+            mondayDrink.SetDrink(favDrink);
+
+            //output the name from the object
+            Console.WriteLine($"My fasvorite morning drink is {mondayDrink.GetDrink()}");
+
+        }//end Main
+    }//End Class DrinkTest
+}//End Namespace
